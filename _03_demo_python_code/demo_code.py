3b38651af5edf5e88224848ffbc0ed172cdcbc3f
Amila Sampath <pgsamila@yahoo.com>
2017-11-26 11:38:28 +0530

    update

--- _03_demo_python_code/demo_code.py
+++ _03_demo_python_code/demo_code.py
@@ -11,26 +11,26 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 #GPIO.setmode(GPIO.BOARD) #use board pin numbering
 #GPIO.setup(5, GPIO.OUT)  #set output pin 5
 #GPIO.setup(26, GPIO.OUT) #set output pin 26
-
+cap = cv2.VideoCapture(0)
 #turn on/off bulbs
 #GPIO.output(5,0) #turn off
 #GPIO.output(26,1)#turn on
 
 #ser.write('1')
-k = 'n'
+#k = 'n'
 
 while 1:
     
     if ser.readline() == 'G':
-        k = 'n'
+        #k = 'n'
         #GPIO.output(5,0)
         #GPIO.output(26,1)
     else:
-        k = 'n'
+        #k = 'n'
         #GPIO.output(5,1)
         #GPIO.output(26,0)
         #open the camera
-        cap = cv2.VideoCapture(0)
+        
         #read camera data
         ret, frame = cap.read()
         #convert to gray image
