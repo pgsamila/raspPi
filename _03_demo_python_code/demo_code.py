cc5f7ba2e3dc8f19d92283fb24009ddd204d3815
Amila Sampath <pgsamila@yahoo.com>
2017-11-26 11:35:24 +0530

    update

--- _03_demo_python_code/demo_code.py
+++ _03_demo_python_code/demo_code.py
@@ -4,17 +4,17 @@
 import numpy as np
 import cv2
 import serial 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 #things setting up
 ser = serial.Serial('/dev/ttyACM0', 9600)
-GPIO.setmode(GPIO.BOARD) #use board pin numbering
-GPIO.setup(5, GPIO.OUT)  #set output pin 5
-GPIO.setup(26, GPIO.OUT) #set output pin 26
+#GPIO.setmode(GPIO.BOARD) #use board pin numbering
+#GPIO.setup(5, GPIO.OUT)  #set output pin 5
+#GPIO.setup(26, GPIO.OUT) #set output pin 26
 
 #turn on/off bulbs
-GPIO.output(5,0) #turn off
-GPIO.output(26,1)#turn on
+#GPIO.output(5,0) #turn off
+#GPIO.output(26,1)#turn on
 
 #ser.write('1')
 k = 'n'
@@ -23,12 +23,12 @@ while 1:
     
     if ser.readline() == 'G':
         k = 'n'
-        GPIO.output(5,0)
-        GPIO.output(26,1)
+        #GPIO.output(5,0)
+        #GPIO.output(26,1)
     else:
         k = 'n'
-        GPIO.output(5,1)
-        GPIO.output(26,0)
+        #GPIO.output(5,1)
+        #GPIO.output(26,0)
         #open the camera
         cap = cv2.VideoCapture(0)
         #read camera data
